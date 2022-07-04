f85c380de2ad3972fdbbd669fbb1cfc5921bdfa5
胡江 <17600112740@163.com>
2022-07-04 10:48:46 +0800

    创建git项目

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,26 @@
+
+/* vite配置文件
+ * title { String } 【 标题 】
+ * description { String } 【 描述 】
+ * appearance { Boolean } 【 是否展示dark主题，默认为true 】
+ * base { String } 【 项目路径，默认为'/' 】
+ * ignoreDeadLinks { Boolean } 【  忽略因为无效链接导致构建失败，默认为false 】
+ * lang { String } 【 页面html lang 语言设置，默认为en-US 】
+ * lastUpdated { Boolean } 【 是否展示上次更新时间，默认为false 】
+ * base { String } 【  】
+ * base { String } 【  】
+ * base { String } 【  】
+ * base { String } 【  】
+ * 
+ * 
+ * 
+*/
+
+const config = {
+  title: 'vite生成文档',
+  description: 'vite文档描述',
+  lastUpdated: true
+}
+
+
+export default config
---
+++ docs/index.md
@@ -0,0 +1,16 @@
+
+
+# Hello VitePress
+
+
+## test1
+
+
+
+## test2
+
+
+### test1-1-1
+
+
+
